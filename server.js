805c5e2f142eab16a362aaedc1f32d714adb0aa5
Lauren Auchter <lsauchter@gmail.com>
2019-07-31 00:14:11 -0400

    Added data filter and header protection

--- server.js
+++ server.js
@@ -1,12 +1,42 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const MOVIES = require('./movies.json');
 
 const app = express();
 
 app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+app.use(function validateBearerToken(req, res, next) {
+    const authToken = req.get('Authorization');
+    const apiToken = process.env.API_TOKEN;
+
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        return res.status(401).json({error: 'Unauthorized access'})
+    }
+
+    next()
+})
 
 function filterMovies(req, res) {
-    res.send('Movies');
+    const {genre, country, avg_vote} = req.query;
+
+    let movieList = MOVIES;
+    if(genre) {
+        movieList = movieList.filter(movie => movie.genre.toLowerCase().includes(genre.toLowerCase()))
+    }
+    if(country) {
+        movieList = movieList.filter(movie => movie.country.toLowerCase().includes(country.toLowerCase()))
+    }
+    if(avg_vote) {
+        const vote = parseFloat(avg_vote);
+        movieList = movieList.filter(movie => movie.avg_vote >= vote)
+    }
+
+    res.send(movieList);
 }
 
 app.get('/movie', filterMovies);
